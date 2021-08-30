2ebc6e61f9da28c567424935d1584676c56e3e44
toshiakifukikoshi <86909359+toshiakifukikoshi@users.noreply.github.com>
2021-08-31 00:10:56 +0900

    page1Routes

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -4,10 +4,12 @@ export const Page1 = () => {
   return (
     <div>
       <h1>ページ１です</h1>
+      <Link to="/page1/DetailA">DetailA</Link>
 
-      <Link to="/page1/detailA">DetailA</Link>
+      {/* <Link to="/page1/detailA">DetailA</Link> */}
       <br />
-      <Link to="/page1/detailB">DetailB</Link>
+      <Link to="/page1/DetailB">DetailB</Link>
+      {/* <Link to="/page1/detailB">DetailB</Link> */}
       <br />
     </div>
   );
