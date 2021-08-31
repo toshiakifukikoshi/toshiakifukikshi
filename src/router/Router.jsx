ad5af969cf896a85e947182ac10b221100c6543f
toshiakifukikoshi <86909359+toshiakifukikoshi@users.noreply.github.com>
2021-08-31 23:49:39 +0900

    Routeの回何とか完了

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,9 +1,7 @@
 import { Switch, Route } from "react-router-dom";
 
 import { Home } from "../Home";
-import { Page1 } from "../Page1";
-import { Page1DetailA } from "../Page1DetailA";
-import { Page1DetailB } from "../Page1DetailB";
+import { Page1Routes } from "./Page1Routes";
 import { Page2 } from "../Page2";
 
 export const Router = () => {
@@ -17,15 +15,15 @@ export const Router = () => {
         path="/page1"
         render={({ match: { url } }) => (
           <Switch>
-            <Route exact path={url}>
-              <Page1 />
-            </Route>
-            <Route path={`${url}/detailA`}>
-              <Page1DetailA />
-            </Route>
-            <Route path={`${url}/detailB`}>
-              <Page1DetailB />
-            </Route>
+            {Page1Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
           </Switch>
         )}
       />
